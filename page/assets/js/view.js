6437cc3be8dbf2bd667f97238c411ecb99d68276
Arthur <a.david220@outlook.fr>
2024-10-26 23:04:11 +0200

    first working version of the gallery

---
+++ page/assets/js/view.js
@@ -0,0 +1,32 @@
+import data from '/photos/data.json' with {type: 'json'};
+
+const photoList = data.photo_list;
+const params = new URLSearchParams(document.location.search);
+const imgName = params.get("img");
+const imgIndex = photoList.indexOf(imgName);
+
+if (imgIndex === -1) {
+    window.location = '/';
+}
+
+document.getElementById('progression').innerHTML = imgIndex+1 + ' / ' + photoList.length;
+document.getElementById("img-display").setAttribute("src", "/photos/"+imgName);
+document.getElementById('close-button').addEventListener('click', function() {
+    window.location = '/#'+imgName;
+});
+document.getElementById('left-arrow-nav').addEventListener('click', function() {
+    if (imgIndex === 0) {
+        window.location = '/view/?img='+photoList[photoList.length-1];
+    } else {
+        window.location = '/view/?img='+photoList[imgIndex-1];
+    }
+});
+document.getElementById('right-arrow-nav').addEventListener('click', function() {
+    if (imgIndex === photoList.length-1) {
+        window.location = '/view/?img='+photoList[0];
+    } else {
+        window.location = '/view/?img='+photoList[imgIndex+1];
+    }
+});
+document.getElementById('download-button').setAttribute("href", "/photos/"+imgName);
+document.getElementById('download-button').setAttribute("download", imgName);
---
+++ setup.py
@@ -0,0 +1,68 @@
+import os
+import json
+from PIL import Image
+from PIL import ExifTags
+from bs4 import BeautifulSoup
+
+PHOTO_DIR = "page/photos/"
+THUMBNAIL_DIR = PHOTO_DIR + "thumbnails/"
+
+os.makedirs(THUMBNAIL_DIR, exist_ok=True)
+
+def get_photos():
+    photos = []
+    for _, _, files in os.walk(PHOTO_DIR):
+        for file in files:
+            if file.endswith(".jpg"):
+                photos.append(file)
+    return photos
+
+def export_photo_list():
+    photos = get_photos()
+    with open(PHOTO_DIR + "data.json", "w") as f:
+        json.dump(photos, f)
+
+def make_thumbnails():
+    photos = get_photos()
+    for photo in photos:
+        img = Image.open(PHOTO_DIR + photo)
+        img.thumbnail((300, 300), Image.BICUBIC)
+        img.save(THUMBNAIL_DIR + photo, quality=95)
+
+def get_exif_data(photo):
+    img = Image.open(PHOTO_DIR+photo)
+    exif_data = img._getexif()
+    if exif_data:
+        exif = {ExifTags.TAGS[k]: v for k, v in exif_data.items() if k in ExifTags.TAGS and type(v) is not bytes}
+        return exif
+    return {}
+
+def edit_html(photos):
+    with open("template.html", "r") as f:
+        soup = BeautifulSoup(f, "html.parser")
+    
+    ul = soup.find("ul", class_="gallery-container")
+    for photo in photos:
+        li = soup.new_tag("li", id=photo)
+        a = soup.new_tag("a", href="/view?img=" + photo)
+        img = soup.new_tag("img", src=THUMBNAIL_DIR[4:] + photo)
+        a.append(img)
+        li.append(a)
+        ul.append(li)
+    
+    with open("page/index.html", "w") as f:
+        f.write(str(soup))
+
+
+if __name__ == "__main__":
+    export_photo_list()
+    photos = get_photos()
+    exif_data = {photo: get_exif_data(photo) for photo in photos}
+    data = {"photo_list": photos} #, "exif_data": exif_data}
+    edit_html(photos)
+    # print(data)
+    with open(PHOTO_DIR + "data.json", "w") as f:
+        json.dump(data, f)
+
+    make_thumbnails()
+
